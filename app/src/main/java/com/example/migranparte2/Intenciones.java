4feac1962cf1dd122934c2ebea866d7e75381501
jorgefloresmadge <jorgefloresmadge@hotmail.com>
2020-06-04 22:13:18 -0500

    Intenciones whatsapp

--- app/src/main/java/com/example/migranparte2/Intenciones.java
+++ app/src/main/java/com/example/migranparte2/Intenciones.java
@@ -1,7 +1,9 @@
 package com.example.migranparte2;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
@@ -47,14 +49,14 @@ public class Intenciones  extends AppCompatActivity {
     }
 
     public void whatsapp(View view) {
-        PackageManager packageManager = this.getPackageManager();
+        PackageManager packageManager = this.getPackageManager();//administrador de paquetes
         Intent i = new Intent(Intent.ACTION_VIEW);
         try {
             String url = "https://api.whatsapp.com/send?phone=" + "+51952000243" + "&text="
                     + URLEncoder.encode("Buen día, tenia una duda del curso ... ", "UTF-8");
-            i.setPackage("com.whatsapp");
-            i.setData(Uri.parse(url));
-            if (i.resolveActivity(packageManager) != null) {
+            i.setPackage("com.whatsapp");//ide de wasap
+            i.setData(Uri.parse(url));//enviamos esta URL
+            if (i.resolveActivity(packageManager) != null) {//SI EXSITE ESTA ACTIVIDAD, SE ENVIA
                 this.startActivity(i);
             }
             else {
@@ -64,5 +66,47 @@ public class Intenciones  extends AppCompatActivity {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public void navegacion(View view) {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setMessage("Seleccione la aplicación");
+        builder.setTitle("Escoja entre Waze o Google Maps");
+        builder.setNeutralButton("Google Map", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                Uri gmmIntentUri;
+                gmmIntentUri = Uri.parse("google.navigation:q=" + -18.013739816 + "," + -70.2510593169);
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                mapIntent.setPackage("com.google.android.apps.maps");
+                if (mapIntent.resolveActivity(getPackageManager()) != null)
+                    startActivity(mapIntent);
+                else
+                    Toast.makeText(Intenciones.this, "Maps no esta instalado", Toast.LENGTH_LONG).show();
+            }
+        });
+        builder.setNegativeButton("Waze", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                Uri gmmIntentUri;
+                gmmIntentUri = Uri.parse("waze://?ll=" + -18.013739816 + "," + -70.2510593169 + "&navigate=yes");
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                mapIntent.setPackage("com.waze");
+                if (mapIntent.resolveActivity(getPackageManager()) != null)
+                    startActivity(mapIntent);
+                else
+                    Toast.makeText(Intenciones.this, "Waze no esta instalado", Toast.LENGTH_LONG).show();
+            }
+        });
+        builder.setPositiveButton("Cancelar", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                return;
+            }
+        });
+        builder.setOnCancelListener(new DialogInterface.OnCancelListener() {
+            public void onCancel(DialogInterface dialog) {
+                return;
+            }
+        });
+        builder.show();
     }
 }
