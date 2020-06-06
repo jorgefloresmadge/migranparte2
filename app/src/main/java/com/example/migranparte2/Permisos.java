5cebad6a70215cb0e410f8d88e3fee24000e49e7
jorgefloresmadge <jorgefloresmadge@hotmail.com>
2020-06-05 20:50:47 -0500

    Añadimos solicitud de permisos de telefono - caso completo

--- app/src/main/java/com/example/migranparte2/Permisos.java
+++ app/src/main/java/com/example/migranparte2/Permisos.java
@@ -9,22 +9,147 @@ import android.Manifest;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class Permisos extends AppCompatActivity {
 
+    //--- un varios permisos
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_permisos);
+        //se brinda o pregunta si tiene LOS permisoS
+        if ((ContextCompat.checkSelfPermission(Permisos.this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) &&
+                (ContextCompat.checkSelfPermission(this,
+                        Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED) &&
+                (ContextCompat.checkSelfPermission(this,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) &&
+                (ContextCompat.checkSelfPermission(this,
+                        Manifest.permission.GET_ACCOUNTS) == PackageManager.PERMISSION_GRANTED)){
+            arranque();
+        } else {
+            Handler handler = new Handler();
+            handler.postDelayed(
+                    new Runnable() {
+                        public void run() {
+                            if (ContextCompat.checkSelfPermission(Permisos.this,
+                                    android.Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
+                                permisolocalizacion();
+                            } else {
+                                solicitarPermiso(Manifest.permission.CALL_PHONE,
+                                        "Sin el permiso" + " de telefono no podemos realizar llamadas.", 0);
+                            }
+                        }
+                    }, 2000L);
+        }
+    }
+
+
+
+
+    void permisolocalizacion(){
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            permisoalmacenamiento();
+        } else {
+            solicitarPermiso(Manifest.permission.ACCESS_FINE_LOCATION,
+                    "Sin el permiso" + " de ubicacion no podremos localizarte", 1);
+        }
+    }
+    void permisoalmacenamiento(){
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            permisocuenta();
+        } else {
+            solicitarPermiso(Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    "Sin el permiso" + " de Almacenamiento no podemos guardar su informacion.", 2);
+        }
+    }
+    void permisocuenta(){
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.GET_ACCOUNTS) == PackageManager.PERMISSION_GRANTED) {
+            arranque();
+        } else {
+            solicitarPermiso(Manifest.permission.GET_ACCOUNTS,
+                    "Sin el permiso" + " de contactos no podemos vincularnos con google o facebook.", 3);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == 0) {
+            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                permisolocalizacion();
+            } else {
+                solicitarPermiso(Manifest.permission.CALL_PHONE,
+                        "Sin el permiso" + " de telefono no podemos realizar llamadas.", 0);
+            }
+        }
+        if (requestCode == 1) {
+            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                permisoalmacenamiento();
+            } else {
+                solicitarPermiso(Manifest.permission.ACCESS_FINE_LOCATION,
+                        "Sin el permiso" + " de ubicacion no podremos localizarte", 1);
+            }
+        }
+        if (requestCode == 2) {
+            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                permisocuenta();
+            } else {
+                solicitarPermiso(Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        "Sin el permiso" + " de Almacenamiento no podemos guardar su informacion.", 2);
+            }
+        }
+        if (requestCode == 3) {
+            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                arranque();
+            } else {
+                solicitarPermiso(Manifest.permission.GET_ACCOUNTS,
+                        "Sin el permiso" + " de cuentas no podemos vincularnos con google o facebook.", 3);
+            }
+        }
+    }
+
+    void arranque() {
+        AlertDialog.Builder dialogo1 = new AlertDialog.Builder(this);
+        dialogo1.setTitle("FELICITACIONES");
+        dialogo1.setMessage("Usted ya tiene todos los permisos necesarios para nuestra app");
+        dialogo1.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialogo1, int id) {
+                finish();
+            }
+        });
+        dialogo1.show();
+    }
+
+    /* --- un solo permiso
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_permisos);
         if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
-            arranque();
-        } else {
+                Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {//se brinda o pregunta si tiene el permiso
+            arranque();//si tiene ejecuta el metodo arranque
+        } else {//si no tiene el permiso muestra ese texto de abajo
+            /*
             solicitarPermiso(Manifest.permission.CALL_PHONE,
-                    "Sin el permiso" + " de telefono no podemos realizar llamadas.", 0);
+                    "Sin el permiso" + " de telefono no podemos realizar llamadas.", 0);//este codigo 0, puede ser cualquier numero
+
+
+            Handler handler = new Handler();
+            handler.postDelayed(//retraso de 2 segundos
+                    new Runnable() {
+                        public void run() {
+                            solicitarPermiso(Manifest.permission.CALL_PHONE,
+                                    "Sin el permiso" + " de telefono no podemos realizar llamadas.", 0);
+                        }
+                    }, 2000L);//2 segundos
         }
     }
-
+    */
+    /*
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         if (requestCode == 0) {
@@ -47,20 +172,21 @@ public class Permisos extends AppCompatActivity {
         });
         dialogo1.show();
     }
-
+    */
+    //ESTE  METODO PIDE EL CHECK
     public void solicitarPermiso(final String permiso, String justificacion, final int codigo) {
         if (ActivityCompat.shouldShowRequestPermissionRationale(this, permiso)) {
-            AlertDialog.Builder dialogo1 = new AlertDialog.Builder(this);
+            AlertDialog.Builder dialogo1 = new AlertDialog.Builder(this);//dialogo del check
             dialogo1.setTitle("Solicitud de permiso");
             dialogo1.setMessage(justificacion);
             dialogo1.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
                 public void onClick(DialogInterface dialogo1, int id) {
-                    ActivityCompat.requestPermissions(Permisos.this, new String[]{permiso}, codigo);
+                    ActivityCompat.requestPermissions(Permisos.this, new String[]{permiso}, codigo);//aparece el dialogo - no se puede personalizar nada
                 }
             });
             dialogo1.show();
         } else {
-            ActivityCompat.requestPermissions(this, new String[]{permiso}, codigo);
+            ActivityCompat.requestPermissions(this, new String[]{permiso}, codigo);// SIN EL CHECK - EN LA 1ERA VEZ(ESTE NO SE PUEDE PERSONALIZAR)
         }
     }
 }
